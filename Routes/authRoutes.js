1c177e9ea41f562a33a3b710e04d02e3be65f9a6
rahulgl-coder <rahulnandhu0521@gmail>
2025-06-21 15:25:08 +0530

    setting up cors 1

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -39,12 +39,17 @@ router.post('/login', async (req, res) => {
     
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ error: 'Invalid credentials' });
+    console.log(user);
+    
 
 
     const isMatch = await bcrypt.compare(password, user.password);
+    console.log(isMatch)
     if (!isMatch) return res.status(400).json({ error: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    console.log(token);
+    
    return res.status(200).json({ token });
   } catch (err) {
     res.status(500).json({ error: 'Server ' });
